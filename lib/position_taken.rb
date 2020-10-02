54bd918b9fd70c70476679b2b1046b554e57ba9e
Chris Benedict <cabx88@gmail.com>
2020-10-02 09:17:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,12 @@
 def position_taken?(board, input)
+<<<<<<< HEAD
   if board[input] == "X" || board[input] == "O"
     return true
   else
     return false
   end
+=======
+  board[input]? == "X" || board[input]? == "O"
+    
+>>>>>>> 2b0c7304043fd3487a1dc252b8c338a7db0c397d
 end
